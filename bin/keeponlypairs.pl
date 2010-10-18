aab0601ffecb1eb4430024540f32febeb1587576
Daniel Nilsson <daniel.nilsson@ki.se>
2010-10-18 13:43:20 +0200

    Changed keeponlypairs a bit to be more readable (and also not discard occasional possible pairs in 1-1-2 transitions needlessly.)

--- bin/keeponlypairs.pl
+++ bin/keeponlypairs.pl
@@ -5,35 +5,44 @@
 
 my $row;
 my $ncount = 0;
-my $bad = 0;
+
 my @r;
 
 my $badcount=0;
 my $total=0;
 
 my $expectedmate = 1;
-my $pair = 1;
+my $potentially_paired = 0;
 
 my $matename = "";
 
 while ( $row = <STDIN> ) {
     if ($ncount == 0) {
-	push @r,$row;
 	$ncount++;
 #	print "DEBUG: ", substr($row, -2);
 
-	if ( substr ($row, -2) ne "$expectedmate\n" ) {
-	    $bad = 1;
-	    if ( $expectedmate == 2 ) {
-		$expectedmate = 1;
+	if ( substr ($row, -2) eq "1\n" ) {
+	    @r=($row);
+		$expectedmate = 2;
+	    if ( $expectedmate == 1 ) {
+		# ok, starting new record.
+	    } else { # expectedmate ==2 , but got a 1 read
+		# replace the previously stored /1 record with the current, and set expectedmate to 2
 	    }
-	} else {
-	    #ok! found correct, bad=0, swap expected 
+	} elsif (substr ($row, -2) eq "2\n" ) {
+	    push @r, $row;
 	    if ($expectedmate == 1) {
-		$expectedmate =2;
-	    } else {
-		$expectedmate =1;		
+                # found a /2 read when waiting for a /1
+                # don't really care about @row,as this entry will be discarded.
+		$badcount++;
+	    } else { #expectedmate==2
+		$expectedmate =1;
+		$potentially_paired=1; #ok! found a potential pair! push row to memory for printing if it holds.
 	    }
+	    
+	} else {
+	    print "something is wrong with the read naming: got a PE named $row";
+	    exit;
 	}
     } elsif ($ncount == 1 || $ncount==2 ) {
 	push @r,$row;
@@ -41,30 +50,26 @@ while ( $row = <STDIN> ) {
     } elsif ($ncount == 3) {
 	$ncount =0;
 	$total++;
-	if ($bad == 0) {
-	    if( $pair == 2) {
-		if ($matename eq substr($r[4],0,-2)) {
-		    print @r, $row;
-		    $pair=1;
-		    @r =();
-		} else {
-		    # actually bad..
-		    $badcount++;
-		    @r=();
-		    $pair=1;
-		}
+	if( $potentially_paired == 1) {
+	    
+	    # check if really named ok
+	    if ($matename eq substr($r[4],0,-2)) {
+		# yes, so print.
+		print @r, $row;
 	    } else {
-		# first mate ok
-		push @r,$row;
-		$pair=2;
-		$matename = substr($r[0],0,-2);
+		# actually bad, although not yet marked so
+		$badcount++;
 	    }
-	} else {
-	    $badcount++;
-	    @r=();
-	    $pair=1;
+	    #lower flag
+	    $potentially_paired=0;
+	    # clear entry..
+	    @r =();
+	} else { #potentially paired ==0
+	    # first mate ok, proceed
+	    push @r,$row;
+#	    $potentially_paired=1; will be set by expectedmate check at row 0..
+	    $matename = substr($r[0],0,-2);
 	}
-	$bad=0;
     }
 
 }
